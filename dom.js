d7cca319e718dca3a3ed94434997bbb617cdb6e4
Infinite00loop <samriddhisingh07092002@gmail.com>
2023-10-01 23:53:18 +0530

    creating node and modifying dom

--- dom.js
+++ dom.js
@@ -29,18 +29,46 @@
 // var xyz=document.getElementsByTagName('li');
 // xyz[4].style.fontWeight='bold';
 
-var item=document.querySelector('.list-group-item:nth-child(2)');
-item.style.color='green';
-var item1=document.querySelector('.list-group-item:nth-child(2)');
-item1.style.display='none';
+// var item=document.querySelector('.list-group-item:nth-child(2)');
+// item.style.color='green';
+// var item1=document.querySelector('.list-group-item:nth-child(2)');
+// item1.style.display='none';
 
-var item2=document.querySelectorAll('.list-group-item');
-item2[1].style.color='green';
+// var item2=document.querySelectorAll('.list-group-item');
+// item2[1].style.color='green';
 
-var odd=document.querySelectorAll('li:nth-child(odd)');
-for(var i=0;i<odd.length;i++){
-    odd[i].style.backgroundColor='green';
-}
+// var odd=document.querySelectorAll('li:nth-child(odd)');
+// for(var i=0;i<odd.length;i++){
+//     odd[i].style.backgroundColor='green';
+// }
+
+var item=document.querySelector('#items');
+console.log(item.parentNode.parentElement.parentElement);
+console.log(item.childNodes);
+console.log(item.children);
+console.log(item.firstElementChild);
+console.log(item.lastElementChild);
+console.log(item.firstChild);
+console.log(item.lastChild);
+console.log(item.nextSibling);
+console.log(item.nextElementSibling);
+console.log(item.previousSibling);
+console.log(item.previousElementSibling);
+var newDiv=document.createElement('div');
+newDiv.className='hello';
+newDiv.id='hola';
+newDiv.setAttribute('title','hello world');
+var text=document.createTextNode('hello');
+newDiv.appendChild(text);
+console.log(newDiv);
+var container=document.querySelector('header .container');
+var h1=document.querySelector('header h1');
+
+container.insertBefore(newDiv,h1);
+
+var item1=document.querySelector('.list-group-item');
+
+item.insertBefore(newDiv,item1);
 
 
 
