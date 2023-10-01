d2b79a2f2705afc17b6c7312d171bea7228a5a4e
Infinite00loop <samriddhisingh07092002@gmail.com>
2023-10-01 20:17:45 +0530

    dom manipulation

--- dom.js
+++ dom.js
@@ -1,1 +1,16 @@
-console.log('this is my first dom file');
+console.dir(document);
+document.title='Module';
+console.log(document.all);
+console.log(document.getElementById('main-header'));
+var title=document.getElementById('header-title');
+// console.log(title);
+// title.innerText='Testing';
+// console.log(title);
+title.innerHTML='<h2>MY HEADING<\h2>';
+var titli=document.getElementById('main-header');
+titli.style.borderBottom='solid 3px #000';
+var abc=document.getElementById('add');
+abc.style.fontWeight='bold';
+abc.style.color='green';
+
+
